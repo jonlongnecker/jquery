1569b77d3bb1e947534b057e9c8d5ef98b42a3d4
Jonathan Longnecker <jon@fortysevenmedia.com>
2012-09-26 13:50:13 -0400

    More work on secondary Page

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
+$(document).ready(function() {
 
+//Slide Form Open////////////////////////////////////////
+
+$('li.toggle-projects').click(function() {$('#global-project-select').slideToggle(000);
+return false;
+});
+
+}); //Close Document Ready
